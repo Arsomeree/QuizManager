835e175455592012f99caa4225fe6a2521a4c252
Reedeveloper <33703509+Arsomeree@users.noreply.github.com>
2023-09-11 09:26:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp1.Data;
+using System.Security.Principal;
+using System.Data.Entity;
+
+namespace WebApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+      var host = CreateHostBuilder(args).Build();
+
+      CreateDbIfNotExists(host);
+
+      host.Run();
+    }
+    private static void CreateDbIfNotExists(IHost host)
+    {
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
+        {
+          var context = services.GetRequiredService<ApplicationDbContext>();
+          DbInitializer.Initialize(context);
+        }
+        catch (Exception ex)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occurred creating the DB.");
+        }
+      }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+  //public static class IdentityExtensions
+  //{
+  //  public static string GetEmailAdress(this IIdentity identity)
+  //  {
+  //    var userId = identity.GetUserId();
+  //    using (var context = new DbContext())
+  //    {
+  //      var user = context.Users.FirstOrDefault(u => u.Id == userId);
+  //      return user.Email;
+  //    }
+  //  }
+  //}
+  }
+  
---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿
+/****** Object: Table [dbo].[Quizinfo] Script Date: 19/09/2021 20:57:58 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Quizinfo] (
+    [id]       INT          NOT NULL,
+    [Title]    VARCHAR (50) NULL,
+    [Question] VARCHAR (50) NULL,
+    [Choice1]  VARCHAR (50) NULL,
+    [Choice2]  VARCHAR (50) NULL,
+    [Choice3]  VARCHAR (50) NULL,
+    [Choice4]  VARCHAR (50) NULL,
+    [Choice5]  VARCHAR (50) NULL,
+    [Answer]   VARCHAR (50) NULL
+);
+
+
